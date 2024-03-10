d01013fa9978b707c4b371797dbe9771330f4072
serniodev43 <serniodev43@gmail.com>
2024-03-10 10:30:39 -0700

    Fix

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -221,12 +221,34 @@ const Navbar = ({ windowWidth }) => {
             Learn
           </a>
 
-          <Button
-            text="Sign up"
-            bgColor="#42b3a2"
-            textColor="white"
-            small={windowWidth < 768}
-          />
+          {pathname !== "/detail" && (
+            <Button
+              text="Sign up"
+              bgColor="#42b3a2"
+              textColor="white"
+              small={windowWidth < 768}
+            />
+          )}
+
+          {pathname === "/detail" && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="28px"
+              height="28px"
+              viewBox="0 0 48 48"
+              className="md:hidden mr-1"
+            >
+              <g
+                fill="none"
+                stroke="black"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <circle cx={17.808} cy={17.804} r={13.041}></circle>
+                <path d="m28.903 32.01l3.225-3.226l9.755 9.755l-3.226 3.225zm1.483-4.963l-3.222 3.223"></path>
+              </g>
+            </svg>
+          )}
 
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -242,44 +264,46 @@ const Navbar = ({ windowWidth }) => {
           </svg>
         </nav>
       </header>
-      <div className="flex md:hidden bg-[#f4eee0] w-full py-2 justify-center gap-10">
-        <div className="flex flex-wrap bg-white items-center w-[70%] rounded-full outline-none px-3 py-2 border-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20px"
-            height="20px"
-            viewBox="0 0 48 48"
-            className="mr-2"
-          >
-            <g
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
+      {pathname !== "/detail" && (
+        <div className="flex md:hidden bg-[#f4eee0] w-full py-2 justify-center gap-10">
+          <div className="flex flex-wrap bg-white items-center w-[70%] rounded-full outline-none px-3 py-2 border-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20px"
+              height="20px"
+              viewBox="0 0 48 48"
+              className="mr-2"
             >
-              <circle cx={17.808} cy={17.804} r={13.041}></circle>
-              <path d="m28.903 32.01l3.225-3.226l9.755 9.755l-3.226 3.225zm1.483-4.963l-3.222 3.223"></path>
-            </g>
-          </svg>
-          <input
-            className="flex-1 outline-none"
-            placeholder="Deschutes River, Oregon"
-          />
+              <g
+                fill="none"
+                stroke="#3E3E3D"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <circle cx={17.808} cy={17.804} r={13.041}></circle>
+                <path d="m28.903 32.01l3.225-3.226l9.755 9.755l-3.226 3.225zm1.483-4.963l-3.222 3.223"></path>
+              </g>
+            </svg>
+            <input
+              className="flex-1 outline-none"
+              placeholder="Deschutes River, Oregon"
+            />
+          </div>
+          <div className="flex items-center justify-center rounded-full w-[40px] h-[40px] bg-[#3E3E3D]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24px"
+              height="24px"
+              viewBox="0 0 28 28"
+            >
+              <path
+                fill="white"
+                d="M17 19a1 1 0 1 1 0 2h-6a1 1 0 1 1 0-2zm4-6a1 1 0 1 1 0 2H7a1 1 0 1 1 0-2zm3-6a1 1 0 1 1 0 2H4a1 1 0 0 1 0-2z"
+              ></path>
+            </svg>
+          </div>
         </div>
-        <div className="flex items-center justify-center rounded-full w-[40px] h-[40px] bg-[#3E3E3D]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24px"
-            height="24px"
-            viewBox="0 0 28 28"
-          >
-            <path
-              fill="white"
-              d="M17 19a1 1 0 1 1 0 2h-6a1 1 0 1 1 0-2zm4-6a1 1 0 1 1 0 2H7a1 1 0 1 1 0-2zm3-6a1 1 0 1 1 0 2H4a1 1 0 0 1 0-2z"
-            ></path>
-          </svg>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
